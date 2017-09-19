4f02d506a756a2cf7b6dbdc729ee9d83cc5a47f2
Azrea <32054416+Azrea@users.noreply.github.com>
2017-09-19 04:39:32 +0200

    changes where made

---
+++ patterns/Pattern2.java
@@ -0,0 +1,17 @@
+
+
+public class Pattern2
+{
+	public static void main(String[] args)
+	{
+		final int SIZE=5;//intialize a constant variable with all CAPS
+		for(int i=1;i<=SIZE;i++)//number of lines to be added
+		{
+			for(int j=SIZE;j>i;j--)//number of * at i line
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
